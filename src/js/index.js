6eb89df09e57ec72650734484898f5d687ff5478
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-07-31 22:18:25 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,8 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Counter from "./component/Counter.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.querySelector("#app"));
